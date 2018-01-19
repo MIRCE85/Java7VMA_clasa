6ba6b00d9e1ddcc60eb8027cb1ec5b64a2e2d70d
Mircea <Mircea@192.168.0.122>
2018-01-19 20:25:52 +0200

    Joaca cu Add

---
+++ JavaTraining/src/Figuri/Point.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Figuri;
+
+/**
+ *
+ * @author Mircea
+ */
+public class Point {
+    
+    public int x = 0;
+    public int y = 0;
+    //constructor
+    public Point(int a, int b) {
+        x = a;
+        y = b;
+    }
+    
+    
+}
